6a39a4798ca67639e30eaf684cbf113fd106d50e
psiddharth930 <psiddharth930@gmail.com>
2026-07-27 12:12:14 +0530

    Update cypress.config.ts

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   experimentalMemoryManagement: true,
   numTestsKeptInMemory: 1,
   viewportWidth: 1920,
-  viewportHeight: 1080,
+  viewportHeight1: 1080,
   e2e: {
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
